731fb8499a347d86aba9537a6b35db71f572eb02
yamacinelli <yagom.ym@gmail.com>
2024-07-25 08:13:43 -0300

    feat(utils): add parse utils

---
+++ simper-api/src/main/java/com/dev/simper/utils/ParseUtils.java
@@ -0,0 +1,56 @@
+package com.dev.simper.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.modelmapper.ModelMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ParseUtils {
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    /**
+     * Method responsible for converting/mapping an object to a specific class
+     * @param sourceObject
+     * @param destinationClass
+     * @return V mapped object
+     * @param <T>
+     * @param <V>
+     */
+    public static <T, V> V parse(T sourceObject, Class<V> destinationClass) {
+        V v = null;
+        try {
+            v = modelMapper.map(sourceObject, destinationClass);
+        } catch (Exception e) {
+            log.error("Unable to convert the object {" + ToStringBuilder.reflectionToString(sourceObject) + "} to the class {" + destinationClass.getSimpleName() + "}.");
+            throw new RuntimeException(e);
+        }
+        return v;
+    }
+
+    /***
+     * Method responsible for converting/mapping a list of objects to a specific class
+     * @param <T>
+     * @param <V>
+     * @param sourceList
+     * @param destinationClass
+     * @return
+     */
+    public static <T, V> List<V> parse(List<T> sourceList, Class<V> destinationClass) {
+        List<V> vList = new ArrayList<V>();
+        if (sourceList == null || sourceList.isEmpty()) {
+            return vList;
+        }
+        for (T t : sourceList) {
+            V v = parse(t, destinationClass);
+            if (v != null) {
+                vList.add(v);
+            }
+        }
+        return vList;
+    }
+}
