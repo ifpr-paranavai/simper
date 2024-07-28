9fd5556cfcfd4d2992e4cc84941ace24fb2bce31
yamacinelli <yagom.ym@gmail.com>
2024-07-28 19:23:49 -0300

    fix(repository): add annotation @Repository

--- simper-api/src/main/java/com/dev/simper/repository/RoleRepository.java
+++ simper-api/src/main/java/com/dev/simper/repository/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.dev.simper.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dev.simper.model.Role;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long>{
     
 }
--- simper-api/src/main/java/com/dev/simper/repository/UserRepository.java
+++ simper-api/src/main/java/com/dev/simper/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.dev.simper.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dev.simper.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
 }
