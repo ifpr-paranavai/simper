6f5970afd9149604fec137b1a4dce1715a16245b
yamacinelli <yagom.ym@gmail.com>
2024-07-28 20:49:50 -0300

    fix: remove not used importations

--- simper-api/src/main/java/com/dev/simper/controller/AuthController.java
+++ simper-api/src/main/java/com/dev/simper/controller/AuthController.java
@@ -1,11 +1,6 @@
 package com.dev.simper.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.simper.dto.AuthRequestDto;
-import com.dev.simper.dto.AuthResponseDto;
-import com.dev.simper.security.JwtTokenUtil;
 import com.dev.simper.service.AuthServiceImpl;
-import com.dev.simper.service.UserDetailsServiceImpl;
 
 
 @RestController
